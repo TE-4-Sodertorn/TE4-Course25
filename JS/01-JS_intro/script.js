c7b5dc2bd38ea1bbf8392b237b7f1a46cf776ff9
Francisco Sabino <fdasabino@gmail.com>
2025-09-15 16:56:37 +0200

    Add initial HTML, CSS, and JavaScript files for introductory template

---
+++ JS/01-JS_intro/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let name = "Frank Sabino";
+console.log(typeof name, name);
+let age = 35;
+console.log(typeof age, age);
+let is_student = false;
+console.log(typeof is_student, is_student);
+
+age = age + 1;
+console.log(age);
+
+is_student = true;
+console.log(is_student);
+name = "Lucas da Silva";
+
+let intro = "My name is" + " " + name + " " + "I am " + age + "...";
+console.log(intro);
