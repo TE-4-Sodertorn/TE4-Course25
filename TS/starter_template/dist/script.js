af70f428523ea5315cb35e2935121e17e6440d50
Francisco Sabino <fdasabino@gmail.com>
2025-10-23 14:00:09 +0200

    Refactor project structure to use TypeScript modules and update build configuration

--- TS/starter_template/dist/script.js
+++ TS/starter_template/dist/script.js
@@ -1,7 +1,5 @@
 import { projectInfo } from "./modules/data.js";
-
 const { name, version, description } = projectInfo;
-
 console.log(`Project: ${name}`);
 console.log(`Version: ${version}`);
 console.log(`About: ${description}`);
--- TS/starter_template/script.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello World");
-var fruits = ["banana", "melon", "coconut"];
-console.log(fruits);
