478e6b9fc55d256c0676d09035f7f8b846e67f56
Francisco Sabino <fdasabino@gmail.com>
2025-11-10 12:41:55 +0100

    Remove unused TypeScript starter template files and configurations

--- TS/starter_template/dist/script.js
+++
@@ -1,5 +0,0 @@
-import { projectInfo } from "./modules/data.js";
-const { name, version, description } = projectInfo;
-console.log(`Project: ${name}`);
-console.log(`Version: ${version}`);
-console.log(`About: ${description}`);
