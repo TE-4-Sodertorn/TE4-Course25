185bb14903881d66775a0579ad2fdb7dff989546
Francisco Sabino <fdasabino@gmail.com>
2025-11-10 13:53:41 +0100

    feat: initial commit

---
+++ TS/starter_ts/script.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+var myName = "Frank";
+var myNumber = 12;
+var message = "".concat(myName, " is ").concat(myNumber, " years old.");
+console.log(message);
---
+++ TS/starter_ts/script.ts
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+let myName: string = "Frank";
+let myNumber: number = 12;
+
+const message = `${myName} is ${myNumber} years old.`;
+
+console.log(message);
